59ec6cd2d3c1f55b80d8b4522bdae7c0c46136c7
Ajay Prajapati <prajapatisani9@gmail.com>
2023-03-04 08:41:16 +0530

    task 3

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import { Button } from 'bootstrap';
 import Signup from './components/Signup';
+import Login from './components/Login';
 
 function App() {
   return (
     <div className="App">
-      <Signup/>
+      {/* <Signup/> */}
+      <Login/>
     </div>
   );
 }
