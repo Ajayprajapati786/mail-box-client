a87cc5891002850804a1b822c1a3e8f6d24200e3
Ajay Prajapati <prajapatisani9@gmail.com>
2023-03-05 02:21:13 +0530

    task 15

--- src/components/Inbox.js
+++ src/components/Inbox.js
@@ -50,31 +50,49 @@ const Inbox = () => {
   };
 
   useEffect(() => {
-    axios
-      .get(
-        `https://mail-box-client-23c51-default-rtdb.firebaseio.com/${sanitizedEmail}/inbox.json`
-      )
-      .then((response) => {
-        console.log(
-          `logging from local inbox ${JSON.stringify(response.data)}`
-        );
-
-        if (response.data) {
-          setMessages(response.data);
-        }
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  },[deleteEmail,setKeyToLocalStorege]);
+    const fetchMessages = () => {
+      axios
+        .get(
+          `https://mail-box-client-23c51-default-rtdb.firebaseio.com/${sanitizedEmail}/inbox.json`
+        )
+        .then((response) => {
+          console.log(
+            `logging from local inbox ${JSON.stringify(response.data)}`
+          );
+  
+          if (response.data) {
+            setMessages(response.data);
+          }
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
+  
+    const intervalId = setInterval(fetchMessages, 500); // fetch messages every 2 seconds
+  
+    // Cleanup function to clear the interval when the component unmounts or when the dependency array changes
+    return () => clearInterval(intervalId);
+  }, [sanitizedEmail, deleteEmail, setKeyToLocalStorege]);
+  
 
   const handleClose = () => {
     setSelectedEmail(null);
   };
 
+
+  const countUnreadMessages = Object.values(messages).reduce((count, message) => {
+    if (message.read === false) {
+      return count + 1;
+    }
+    return count;
+  }, 0);
+  
+//   console.log(`There are ${countUnreadMessages} unread messages.`);
+
   return (
     <div>
-      <h3>Inbox</h3>
+      <h3>Inbox {`There are ${countUnreadMessages} unread messages.`}</h3>
       <Card className="text-left">
         <ListGroup variant="flush">
           {Object.keys(messages).reverse().map((key, index) => (
